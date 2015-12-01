436688e695cbe89d43bdb54515951b4b48e86ff2
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2015-12-01 17:22:12 +1300

    Changed: Postgres is stricter than sqlite.  Minor changes for compatability.
    I changed circa type from boolean to interger.
    Also increased the max string length for vol_page and edition from 10 to 50. That
    is probably more than needed, but I wanted to be safe.

--- DictionaryOfNewZealandEnglish/headword/citation/models.py
+++ DictionaryOfNewZealandEnglish/headword/citation/models.py
@@ -19,51 +19,51 @@ citation_source = db.Table('citation_source',
 
 
 class Citation(SurrogatePK, Model):
-    
+
     __tablename__ = "citations"
     date =       Column(db.DateTime,   nullable=False)
     # per expert, circa needed as not all dates are accurate
-    circa =      Column(db.Boolean,      default=False)
+    circa =      Column(db.Integer,      default=False)
     author =     Column(db.String(80),   nullable=False)
 
     source_id =  ReferenceCol('sources', nullable=True)
     source =     relationship('Source',  order_by="Source.name", backref='citations')
 
-    vol_page =   Column(db.String(10),   nullable=True)
-    edition =    Column(db.String(10),   nullable=True)
+    vol_page =   Column(db.String(50),   nullable=True)
+    edition =    Column(db.String(50),   nullable=True)
     quote =      Column(db.Text,         nullable=True)
     notes =      Column(db.Text,         nullable=True)
     archived =   Column(db.Boolean,      default=False)
-    
+
     created_at = Column(db.DateTime,     default=dt.datetime.utcnow)
     updated_at = Column(db.DateTime,     nullable=False)
     updated_by = Column(db.String(80),   nullable=False)
 
 
     def __init__(self, date,
-                       circa, 
-                       author, 
-                       source_id, 
-                       vol_page, 
+                       circa,
+                       author,
+                       source_id,
+                       vol_page,
                        edition,
-                       quote, 
-                       notes, 
-                       archived, 
+                       quote,
+                       notes,
+                       archived,
  										   updated_at,
                        updated_by
                        ):
 
         db.Model.__init__(self, date              = date,
-                                circa             = circa, 
-                                author            = author, 
-                                source_id         = source_id, 
-                                vol_page          = vol_page, 
+                                circa             = circa,
+                                author            = author,
+                                source_id         = source_id,
+                                vol_page          = vol_page,
                                 edition           = edition,
-                                quote             = quote, 
-                                notes             = notes, 
-                                archived          = archived, 
+                                quote             = quote,
+                                notes             = notes,
+                                archived          = archived,
                                 updated_at        = updated_at,
-                                updated_by        = updated_by 
+                                updated_by        = updated_by
                                 )
 
     @property
@@ -76,4 +76,3 @@ class Citation(SurrogatePK, Model):
 
     def __repr__(self):
         return '<Citation ({0}, {1})>'.format(self.author, self.date)
-
