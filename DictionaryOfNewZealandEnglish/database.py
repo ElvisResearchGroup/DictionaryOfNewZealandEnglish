e78b84eff3946746c93fce179bcac5e5846f4855
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2015-12-07 16:30:56 +1300

    Removed: Hardcoded SQLite references and functions in database.py
    They appear to no longer be needed.

--- DictionaryOfNewZealandEnglish/database.py
+++ DictionaryOfNewZealandEnglish/database.py
@@ -11,22 +11,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from .extensions import db
 from .compat import basestring
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-
-Base = declarative_base()
-Base.query = db_session.query_property()
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import DictionaryOfNewZealandEnglish.user.models
-
-    Base.metadata.create_all(bind=engine)
-
 # Alias common SQLAlchemy names
 Column = db.Column
 relationship = relationship
@@ -95,4 +79,3 @@ def ReferenceCol(tablename, nullable=False, pk_name='id', **kwargs):
     return db.Column(
         db.ForeignKey("{0}.{1}".format(tablename, pk_name)),
         nullable=nullable, **kwargs)
-
